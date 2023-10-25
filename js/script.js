f2e9a5e22070b034eba6492d0e2b7365a19c1a66
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-10-25 14:39:01 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let menuToggle = document.querySelector('.menu').addEventListener('click',()=>{
+    let overlay = document.querySelector('.overlay');
+    overlay.classList.toggle('left-0');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./**/*.{html,js}"
+           ],
+  theme: {
+    fontFamily: {
+      "inter": ['Inter', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
